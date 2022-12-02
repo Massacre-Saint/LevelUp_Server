1e74d1bc6f7b23848fc2e6c72eb3172db1894891
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-12-01 18:34:56 -0600

    Chapter 8

--- levelupapi/views/event_view.py
+++ levelupapi/views/event_view.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Event
+from levelupapi.models import Event, Game, Gamer
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -45,4 +45,23 @@ class EventView(ViewSet):
             
         serializer = EventSerializer(events, many=True)
         
+        return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        gamer = Gamer.objects.get(id=request.data["organizer"])
+        game = Game.objects.get(pk=request.data["game"])
+
+        event = Event.objects.create(
+            description=request.data["description"],
+            date=request.data["date"],
+            time=request.data["time"],
+            game=game,
+            organizer=gamer
+        )
+        serializer = EventSerializer(event)
         return Response(serializer.data)
